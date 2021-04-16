f02035e49009d3a7d1e2e878f8b5e8a1b688f1db
Emiel <emiel.delaey@live.be>
2021-04-16 20:57:07 +0200

    dcfd

--- scripts/contact.js
+++ scripts/contact.js
@@ -31,5 +31,24 @@ function isElementInViewport (el) {
       rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
   );
 }
-
+var form = document.getElementById("contactForm");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("contactForm-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Bedankt voor bericht!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oeps! Er was een probleem met het versturen van het bericht."
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
 
