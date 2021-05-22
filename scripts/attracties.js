651351eaa13b07f06f471001cbb19d12000c9d25
Emiel <emiel.delaey@live.be>
2021-05-22 11:17:29 +0200

    f

--- scripts/attracties.js
+++ scripts/attracties.js
@@ -32,145 +32,59 @@ function isElementInViewport (el) {
   );
 }
 
-$(document).ready(function() {
-  $('#bluefire').magnificPopup({
-    items: {
-      src: 'https://www.youtube.com/watch?v=P7m8SNRj_hI'
-    },
-    type:'iframe',
-    iframe: {
-      markup: '<div class="mfp-iframe-scaler">'+
-              '<div class="mfp-close"></div>'+
-              '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-              '</div>', 
-      patterns: {
-          youtube: {
-              index: 'youtube.com/', 
-              id: 'v=', 
-              src: '//www.youtube.com/embed/%id%?autoplay=1&hl=nl&modestbranding=1&' 
-          }
-       },
-       srcAction: 'iframe_src', 
-   }
-  });
-  $('#wodan').magnificPopup({
-    items: {
-      src: 'https://www.youtube.com/watch?v=Ct5kx-wFCos'
-    },
-    type:'iframe',
-    iframe: {
-      markup: '<div class="mfp-iframe-scaler">'+
-              '<div class="mfp-close"></div>'+
-              '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-              '</div>', 
-      patterns: {
-          youtube: {
-              index: 'youtube.com/', 
-              id: 'v=', 
-              src: '//www.youtube.com/embed/%id%?autoplay=1&hl=nl&modestbranding=1&end=100' 
-          }
-       },
-       srcAction: 'iframe_src', 
-   }
-  });
-  $('#silverstar').magnificPopup({
-    items: {
-      src: 'https://www.youtube.com/watch?v=S_rAf6iPZ_A'
-    },
-    type:'iframe',
-    iframe: {
-      markup: '<div class="mfp-iframe-scaler">'+
-              '<div class="mfp-close"></div>'+
-              '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-              '</div>', 
-      patterns: {
-          youtube: {
-              index: 'youtube.com/', 
-              id: 'v=', 
-              src: '//www.youtube.com/embed/%id%?autoplay=1&hl=nl&modestbranding=1&start=55&end=135' 
-          }
-       },
-       srcAction: 'iframe_src', 
-   }
-  });
-  $('#poseidon').magnificPopup({
-    items: {
-      src: 'https://www.youtube.com/watch?v=XiEUx2NsK7c'
-    },
-    type:'iframe',
-    iframe: {
-      markup: '<div class="mfp-iframe-scaler">'+
-              '<div class="mfp-close"></div>'+
-              '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-              '</div>', 
-      patterns: {
-          youtube: {
-              index: 'youtube.com/', 
-              id: 'v=', 
-              src: '//www.youtube.com/embed/%id%?autoplay=1&hl=nl&modestbranding=1&start=45&end=366' 
-          }
-       },
-       srcAction: 'iframe_src', 
-   }
-  });
-  $('#supersplash').magnificPopup({
-    items: {
-      src: 'https://www.youtube.com/watch?v=Eb9cBg0ATpc'
-    },
-    type:'iframe',
-    iframe: {
-      markup: '<div class="mfp-iframe-scaler">'+
-              '<div class="mfp-close"></div>'+
-              '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-              '</div>', 
-      patterns: {
-          youtube: {
-              index: 'youtube.com/', 
-              id: 'v=', 
-              src: '//www.youtube.com/embed/%id%?autoplay=1&hl=nl&modestbranding=1' 
-          }
-       },
-       srcAction: 'iframe_src', 
-   }
-  });
-  $('#alpenexpress').magnificPopup({
-    items: {
-      src: 'https://www.youtube.com/watch?v=8zFBUSsN-Rc'
-    },
-    type:'iframe',
-    iframe: {
-      markup: '<div class="mfp-iframe-scaler">'+
-              '<div class="mfp-close"></div>'+
-              '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-              '</div>', 
-      patterns: {
-          youtube: {
-              index: 'youtube.com/', 
-              id: 'v=', 
-              src: '//www.youtube.com/embed/%id%?autoplay=1&hl=nl&modestbranding=1&start=5&end=110' 
-          }
-       },
-       srcAction: 'iframe_src', 
-   }
-  });
-  $('#alpenexpressvr').magnificPopup({
-    items: {
-      src: 'https://www.youtube.com/watch?v=3xnnhVsRz_4'
-    },
-    type:'iframe',
-    iframe: {
-      markup: '<div class="mfp-iframe-scaler">'+
-              '<div class="mfp-close"></div>'+
-              '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-              '</div>', 
-      patterns: {
-          youtube: {
-              index: 'youtube.com/', 
-              id: 'v=', 
-              src: '//www.youtube.com/embed/%id%?autoplay=1&hl=nl&modestbranding=1&end=101' 
-          }
-       },
-       srcAction: 'iframe_src', 
-   }
-  });
+$(".video-no-end").modalVideo({
+  youtube:{
+    controls:1,
+    nocookie: true,
+            autoplay: 1,
+            modestbranding: 1,
+  }
+});
+    $("#wodan").modalVideo({
+  youtube:{
+    controls:1,
+    nocookie: true,
+            autoplay: 1,
+            modestbranding: 1,
+            end: 100
+  }
+});
+    $("#silverstar").modalVideo({
+  youtube:{
+    controls:1,
+    nocookie: true,
+            autoplay: 1,
+            modestbranding: 1,
+            start:55,
+            end: 135
+  }
+});
+    $("#poseidon").modalVideo({
+  youtube:{
+    controls:1,
+    nocookie: true,
+            autoplay: 1,
+            modestbranding: 1,
+            start:45,
+            end: 366
+  }
+});
+    $("#alpenexpress").modalVideo({
+  youtube:{
+    controls:1,
+    nocookie: true,
+            autoplay: 1,
+            modestbranding: 1,
+            start:5,
+            end: 110
+  }
+});
+    $("#alpenexpressvr").modalVideo({
+  youtube:{
+    controls:1,
+    nocookie: true,
+            autoplay: 1,
+            modestbranding: 1,
+            end: 101
+  }
 });
